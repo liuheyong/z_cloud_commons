cbe136b356265ac5817214bbf9fb6f68afe92f9a
liuheyong <17682347237@163.com>
2019-06-09 23:45:20 +0800

    commit

--- src/main/java/com/cloud/commons/constants/Constants.java
+++ src/main/java/com/cloud/commons/constants/Constants.java
@@ -6,4 +6,28 @@ package com.cloud.commons.constants;
  * @description: Constants
  **/
 public class Constants {
+
+    /**
+     * 成功
+     */
+    public static final String RESULT_SUCCESS = "1";
+    /**
+     * 失败
+     */
+    public static final String RESULT_FAIL = "0";
+    /**
+     * boot标识
+     */
+    public static final String CLOUD = "cloud";
+
+    /**
+     * 统一异常跳转页面
+     */
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 100
+     */
+    public static final Integer NUMBER_100 = 100;
+
 }
